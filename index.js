9a886cc07aa3a4373e78d569e753e87bbebba747
Johan Lindell <johan@lindell.me>
2019-04-02 18:29:45 +0200

    Added consultant work

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var countries = require('./resources/countryCodes.json');
 function render(resume) {
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
 
-  var printCSS = fs.readFileSync(__dirname + "/css/print.css", "utf-8");
+	var printCSS = fs.readFileSync(__dirname + "/css/print.css", "utf-8");
 	var standardCSS = fs.readFileSync(__dirname + "/css/style.css", "utf-8");
 	var screenCSS = fs.readFileSync(__dirname + "/css/screen.css", "utf-8");
 
@@ -15,7 +15,7 @@ function render(resume) {
 	// Remove http and www from url
 	Handlebars.registerHelper('no-http', function(options) {
 		this.url = this.url.replace(/(https?:\/\/)?(www\.)?/, "");
-	  return options.fn(this);
+		return options.fn(this);
 	});
 
 	// Get a font awsome class from a name
@@ -23,26 +23,26 @@ function render(resume) {
 		return "fa-" + str.toLowerCase() + "-square";
 	});
 
-  // http://stackoverflow.com/a/31632215/838789
+	// http://stackoverflow.com/a/31632215/838789
 	Handlebars.registerHelper({
-	    and: function (v1, v2) {
-	        return v1 && v2;
-	    },
-	    or: function (v1, v2) {
-	        return v1 || v2;
-	    }
+			and: function (v1, v2) {
+					return v1 && v2;
+			},
+			or: function (v1, v2) {
+					return v1 || v2;
+			}
 	});
 
-  // http://stackoverflow.com/a/18831911
-  Handlebars.registerHelper('commalist', function(items, options) {
-    return options.fn(items.join(', '));
-  });
+	// http://stackoverflow.com/a/18831911
+	Handlebars.registerHelper('commalist', function(items, options) {
+		return options.fn(items.join(', '));
+	});
 
-  // Send all necessary resources to the handlebars template and compile it
+	// Send all necessary resources to the handlebars template and compile it
 	return Handlebars.compile(template)({
 		resume: resume,
 		standardCSS: standardCSS,
-    printCSS: printCSS,
+		printCSS: printCSS,
 		screenCSS: screenCSS
 	});
 }
